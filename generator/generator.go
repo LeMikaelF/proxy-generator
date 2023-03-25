526a1aa9166e7a7c18292b8f199b26f04a072d27
Mikaël Francoeur <mikael.francoeur@hotmail.com>
2023-03-25 19:57:34 -0400

    Extract generator to a separate type.

--- generator/generator.go
+++ generator/generator.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"flag"
 	"fmt"
+	"github.com/LeMikaelF/proxy-generator/generator/internal/method"
 	"go/ast"
 	"go/format"
 	"go/parser"
@@ -55,24 +56,11 @@ func getWorkingDir() (string, error) {
 	return wd, nil
 }
 
-type method struct {
-	Name                         string
-	Params                       string
-	Results                      string
-	ParamNames                   string
-	ParamNamesWithTypeAssertions string
-	Receiver                     string
-	ResultTypes                  []string
-	ParamTypes                   []*ast.Field
-	ResultExprs                  []*ast.Field
-	Passthrough                  bool
-}
-
 type data struct {
 	PackageName string
 	StructName  string
 	ProxyName   string
-	Methods     []method
+	Methods     []method.Method
 	Imports     []string
 }
 
@@ -88,7 +76,7 @@ func (g *Generator) Run() error {
 	fset := token.NewFileSet()
 
 	var structDecl *ast.GenDecl
-	var methods []method
+	var methods []method.Method
 	var packageName string
 	imports := make(map[string]struct{})
 
@@ -130,7 +118,7 @@ func (g *Generator) Run() error {
 	return nil
 }
 
-func (g *Generator) generateCode(packageName string, methods []method, imports map[string]struct{}) ([]byte, error) {
+func (g *Generator) generateCode(packageName string, methods []method.Method, imports map[string]struct{}) ([]byte, error) {
 	var buf bytes.Buffer
 	err := template.Must(template.New("proxy").Parse(proxyTemplate)).
 		Execute(&buf, data{
@@ -216,8 +204,8 @@ func findStructDeclaration(fileNode ast.Node, typeName string) *ast.GenDecl {
 	return structDecl
 }
 
-func findMethods(fileNode ast.Node, structName string, passThroughMethods map[string]bool) ([]method, []string) {
-	var methods []method
+func findMethods(fileNode ast.Node, structName string, passThroughMethods map[string]bool) ([]method.Method, []string) {
+	var methods []method.Method
 	imports := make(map[string]struct{})
 	importMap := collectImports(fileNode)
 
@@ -234,7 +222,7 @@ func findMethods(fileNode ast.Node, structName string, passThroughMethods map[st
 			return true
 		}
 
-		m := newMethod(passThroughMethods, funcDecl, ident, hasStar)
+		m := method.New(passThroughMethods, funcDecl, ident, hasStar)
 		methods = append(methods, m)
 
 		populateImports(imports, m, importMap)
@@ -245,23 +233,34 @@ func findMethods(fileNode ast.Node, structName string, passThroughMethods map[st
 	return methods, mapToSlice(imports)
 }
 
-func newMethod(passThroughMethods map[string]bool, funcDecl *ast.FuncDecl, ident *ast.Ident, hasStar bool) method {
-	m := method{}
-	populatePassthrough(&m, passThroughMethods[funcDecl.Name.Name])
-	populateName(&m, funcDecl)
-	populateReceiver(&m, ident.Name, hasStar)
-	populateParameters(&m, funcDecl)
-	populateResults(&m, funcDecl)
-	return m
-}
-
-func populatePassthrough(m *method, isPassthrough bool) {
-	m.Passthrough = isPassthrough
-}
-
 type importInfo struct {
 	Path  string
 	Alias string
+}
+
+func populateImports(imports map[string]struct{}, m method.Method, importMap map[string]importInfo) {
+	for _, field := range append(m.ParamTypes, m.ResultExprs...) {
+		switch t := field.Type.(type) {
+		case *ast.Ident:
+			if info, ok := importMap[t.Name]; ok {
+				imports[fmt.Sprintf("%s %q", info.Alias, info.Path)] = struct{}{}
+			}
+		case *ast.SelectorExpr:
+			if x, ok := t.X.(*ast.Ident); ok {
+				if info, ok := importMap[x.Name]; ok {
+					imports[fmt.Sprintf("%s %q", info.Alias, info.Path)] = struct{}{}
+				}
+			}
+		case *ast.StarExpr:
+			if se, ok := t.X.(*ast.SelectorExpr); ok {
+				if x, ok := se.X.(*ast.Ident); ok {
+					if info, ok := importMap[x.Name]; ok {
+						imports[fmt.Sprintf("%s %q", info.Alias, info.Path)] = struct{}{}
+					}
+				}
+			}
+		}
+	}
 }
 
 func collectImports(fileNode ast.Node) map[string]importInfo {
@@ -290,15 +289,6 @@ func collectImports(fileNode ast.Node) map[string]importInfo {
 	return importMap
 }
 
-func getReceiver(funcDecl *ast.FuncDecl) (ast.Expr, bool) {
-	recvType := funcDecl.Recv.List[0].Type
-	starExpr, isStar := recvType.(*ast.StarExpr)
-	if isStar {
-		recvType = starExpr.X
-	}
-	return recvType, isStar
-}
-
 func mapToSlice(imports map[string]struct{}) []string {
 	importsList := make([]string, 0, len(imports))
 	for k := range imports {
@@ -307,151 +297,11 @@ func mapToSlice(imports map[string]struct{}) []string {
 	return importsList
 }
 
-func populateName(m *method, funcDecl *ast.FuncDecl) {
-	m.Name = funcDecl.Name.Name
-}
-
-func populateReceiver(m *method, receiver string, hasStar bool) {
-	var starPrefix string
-	if hasStar {
-		starPrefix = "*"
+func getReceiver(funcDecl *ast.FuncDecl) (ast.Expr, bool) {
+	recvType := funcDecl.Recv.List[0].Type
+	starExpr, isStar := recvType.(*ast.StarExpr)
+	if isStar {
+		recvType = starExpr.X
 	}
-	m.Receiver = fmt.Sprintf("%s%s", starPrefix, receiver)
-}
-
-func populateParameters(m *method, funcDecl *ast.FuncDecl) {
-	if funcDecl.Type.Params != nil {
-		m.Params = parameterStrings(funcDecl.Type.Params.List)
-		m.ParamNames = fieldNamesCommaDelimited(funcDecl.Type.Params.List)
-		m.ParamNamesWithTypeAssertions = fieldNamesWithTypeAssertions(funcDecl.Type.Params.List)
-		m.ParamTypes = funcDecl.Type.Params.List
-	}
-}
-
-func populateResults(m *method, funcDecl *ast.FuncDecl) {
-	if funcDecl.Type.Results != nil {
-		m.Results = returnParametersString(funcDecl.Type.Results.List)
-		m.ResultTypes = typeNames(funcDecl.Type.Results.List)
-		m.ResultExprs = funcDecl.Type.Results.List
-	}
-}
-
-func populateImports(imports map[string]struct{}, m method, importMap map[string]importInfo) {
-	for _, field := range append(m.ParamTypes, m.ResultExprs...) {
-		switch t := field.Type.(type) {
-		case *ast.Ident:
-			if info, ok := importMap[t.Name]; ok {
-				imports[fmt.Sprintf("%s %q", info.Alias, info.Path)] = struct{}{}
-			}
-		case *ast.SelectorExpr:
-			if x, ok := t.X.(*ast.Ident); ok {
-				if info, ok := importMap[x.Name]; ok {
-					imports[fmt.Sprintf("%s %q", info.Alias, info.Path)] = struct{}{}
-				}
-			}
-		case *ast.StarExpr:
-			if se, ok := t.X.(*ast.SelectorExpr); ok {
-				if x, ok := se.X.(*ast.Ident); ok {
-					if info, ok := importMap[x.Name]; ok {
-						imports[fmt.Sprintf("%s %q", info.Alias, info.Path)] = struct{}{}
-					}
-				}
-			}
-		}
-	}
-}
-
-func fieldNamesWithTypeAssertions(fields []*ast.Field) string {
-	var namesWithTypeAssertions []string
-
-	for _, field := range fields {
-		typeExpr := getTypeString(field.Type)
-		for range field.Names {
-			namesWithTypeAssertions = append(namesWithTypeAssertions, fmt.Sprintf("args[%d].(%s)", len(namesWithTypeAssertions), typeExpr))
-		}
-	}
-
-	return strings.Join(namesWithTypeAssertions, ", ")
-}
-
-func typeName(expr ast.Expr) string {
-	switch t := expr.(type) {
-	case *ast.Ident:
-		return t.Name
-	case *ast.SelectorExpr:
-		return fmt.Sprintf("%s.%s", typeName(t.X), t.Sel.Name)
-	case *ast.StarExpr:
-		return "*" + typeName(t.X)
-	case *ast.ArrayType:
-		return "[]" + typeName(t.Elt)
-	case *ast.MapType:
-		return fmt.Sprintf("map[%s]%s", typeName(t.Key), typeName(t.Value))
-	case *ast.InterfaceType:
-		return "any"
-	case *ast.ChanType:
-		return "chan " + typeName(t.Value)
-	default:
-		panic(fmt.Sprintf("could not infer name for type %T", t))
-	}
-}
-
-func getTypeString(expr ast.Expr) string {
-	return typeName(expr)
-}
-
-func parameterStrings(fields []*ast.Field) string {
-	parts := make([]string, 0, len(fields))
-	for _, field := range fields {
-		parts = append(parts, fmt.Sprintf("%s %s", strings.Join(names(field.Names), " "), getTypeString(field.Type)))
-	}
-	return strings.Join(parts, ", ")
-}
-
-func returnParametersString(fields []*ast.Field) string {
-	parts := make([]string, 0, len(fields))
-	for _, field := range fields {
-		parts = append(parts, fmt.Sprintf("%s %s", strings.Join(names(field.Names), " "), getTypeString(field.Type)))
-	}
-
-	joined := strings.Join(parts, ", ")
-
-	if len(fields) > 1 {
-		joined = "(" + joined + ")"
-	}
-
-	return joined
-}
-
-func fieldNamesCommaDelimited(fields []*ast.Field) string {
-	names := fieldNames(fields)
-	return strings.Join(names, ", ")
-}
-
-func fieldNames(fields []*ast.Field) []string {
-	var names []string
-	for _, field := range fields {
-		for _, name := range field.Names {
-			names = append(names, name.Name)
-		}
-	}
-	return names
-}
-
-func names(idents []*ast.Ident) []string {
-	parts := make([]string, 0, len(idents))
-
-	for _, ident := range idents {
-		parts = append(parts, ident.Name)
-	}
-	return parts
-}
-
-func typeNames(idents []*ast.Field) []string {
-	typeNames := make([]string, 0, len(idents))
-
-	for _, ident := range idents {
-		typeNames = append(typeNames, typeName(ident.Type))
-	}
-
-	return typeNames
+	return recvType, isStar
 }
---
+++ generator/internal/method/method.go
@@ -0,0 +1,154 @@
+package method
+
+import (
+	"fmt"
+	"go/ast"
+	"strings"
+)
+
+type Method struct {
+	Name                         string
+	Params                       string
+	Results                      string
+	ParamNames                   string
+	ParamNamesWithTypeAssertions string
+	Receiver                     string
+	ResultTypes                  []string
+	ParamTypes                   []*ast.Field
+	ResultExprs                  []*ast.Field
+	Passthrough                  bool
+}
+
+func New(passThroughMethods map[string]bool, funcDecl *ast.FuncDecl, ident *ast.Ident, hasStar bool) Method {
+	m := Method{}
+	populatePassthrough(&m, passThroughMethods[funcDecl.Name.Name])
+	populateName(&m, funcDecl)
+	populateReceiver(&m, ident.Name, hasStar)
+	populateParameters(&m, funcDecl)
+	populateResults(&m, funcDecl)
+	return m
+}
+
+func populatePassthrough(m *Method, isPassthrough bool) {
+	m.Passthrough = isPassthrough
+}
+
+func populateName(m *Method, funcDecl *ast.FuncDecl) {
+	m.Name = funcDecl.Name.Name
+}
+
+func populateReceiver(m *Method, receiver string, hasStar bool) {
+	var starPrefix string
+	if hasStar {
+		starPrefix = "*"
+	}
+	m.Receiver = fmt.Sprintf("%s%s", starPrefix, receiver)
+}
+
+func populateParameters(m *Method, funcDecl *ast.FuncDecl) {
+	if funcDecl.Type.Params != nil {
+		m.Params = parameterStrings(funcDecl.Type.Params.List)
+		m.ParamNames = fieldNamesCommaDelimited(funcDecl.Type.Params.List)
+		m.ParamNamesWithTypeAssertions = fieldNamesWithTypeAssertions(funcDecl.Type.Params.List)
+		m.ParamTypes = funcDecl.Type.Params.List
+	}
+}
+
+func populateResults(m *Method, funcDecl *ast.FuncDecl) {
+	if funcDecl.Type.Results != nil {
+		m.Results = returnParametersString(funcDecl.Type.Results.List)
+		m.ResultTypes = typeNames(funcDecl.Type.Results.List)
+		m.ResultExprs = funcDecl.Type.Results.List
+	}
+}
+
+func fieldNamesWithTypeAssertions(fields []*ast.Field) string {
+	var namesWithTypeAssertions []string
+
+	for _, field := range fields {
+		typeExpr := typeName(field.Type)
+		for range field.Names {
+			namesWithTypeAssertions = append(namesWithTypeAssertions, fmt.Sprintf("args[%d].(%s)", len(namesWithTypeAssertions), typeExpr))
+		}
+	}
+
+	return strings.Join(namesWithTypeAssertions, ", ")
+}
+
+func parameterStrings(fields []*ast.Field) string {
+	parts := make([]string, 0, len(fields))
+	for _, field := range fields {
+		parts = append(parts, fmt.Sprintf("%s %s", strings.Join(names(field.Names), " "), typeName(field.Type)))
+	}
+	return strings.Join(parts, ", ")
+}
+
+func returnParametersString(fields []*ast.Field) string {
+	parts := make([]string, 0, len(fields))
+	for _, field := range fields {
+		parts = append(parts, fmt.Sprintf("%s %s", strings.Join(names(field.Names), " "), typeName(field.Type)))
+	}
+
+	joined := strings.Join(parts, ", ")
+
+	if len(fields) > 1 {
+		joined = "(" + joined + ")"
+	}
+
+	return joined
+}
+
+func fieldNamesCommaDelimited(fields []*ast.Field) string {
+	names := fieldNames(fields)
+	return strings.Join(names, ", ")
+}
+
+func typeNames(idents []*ast.Field) []string {
+	typeNames := make([]string, 0, len(idents))
+
+	for _, ident := range idents {
+		typeNames = append(typeNames, typeName(ident.Type))
+	}
+
+	return typeNames
+}
+
+func typeName(expr ast.Expr) string {
+	switch t := expr.(type) {
+	case *ast.Ident:
+		return t.Name
+	case *ast.SelectorExpr:
+		return fmt.Sprintf("%s.%s", typeName(t.X), t.Sel.Name)
+	case *ast.StarExpr:
+		return "*" + typeName(t.X)
+	case *ast.ArrayType:
+		return "[]" + typeName(t.Elt)
+	case *ast.MapType:
+		return fmt.Sprintf("map[%s]%s", typeName(t.Key), typeName(t.Value))
+	case *ast.InterfaceType:
+		return "any"
+	case *ast.ChanType:
+		return "chan " + typeName(t.Value)
+	default:
+		panic(fmt.Sprintf("could not infer name for type %T", t))
+	}
+}
+
+func fieldNames(fields []*ast.Field) []string {
+	var names []string
+	for _, field := range fields {
+		for _, name := range field.Names {
+			names = append(names, name.Name)
+		}
+	}
+	return names
+}
+
+func names(idents []*ast.Ident) []string {
+	parts := make([]string, 0, len(idents))
+
+	for _, ident := range idents {
+		parts = append(parts, ident.Name)
+	}
+	return parts
+}
